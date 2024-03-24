4a46a94c48df8fca6f08aaccdf7572dda692f51d
NguyenBaoUyen <145897661+NguyenBaoUyen@users.noreply.github.com>
2024-03-24 17:23:46 +0700

    Tiến Hành 5

--- Cau1_QuanLySV/app/src/main/java/ntu/nguyenbaouyen63131662/MainActivity.java
+++ Cau1_QuanLySV/app/src/main/java/ntu/nguyenbaouyen63131662/MainActivity.java
@@ -2,6 +2,7 @@ package ntu.nguyenbaouyen63131662;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
@@ -10,12 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
+import ntu.nguyenbaouyen63131662.adapter.AdapterSinhVien;
+import ntu.nguyenbaouyen63131662.model.SinhVien;
+
 public class MainActivity extends AppCompatActivity {
 
+    ListView lv_HienThiDS;
+    ArrayList<SinhVien> dsSinhVien = new ArrayList<>();
+    AdapterSinhVien adapterSinhVien;
+    //Tạo một
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addcontrols();
+        addEvent();
     }
 
     @Override
@@ -23,5 +35,28 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Quản Quản Lý Sinh Viên");
         return super.onCreateOptionsMenu(menu);
+
+
+    }
+
+    private void addEvent() {
+    }
+
+    private void addcontrols(){
+        lv_HienThiDS = findViewById(R.id.dsHienThi);
+        dsSinhVien.add(new SinhVien("SV1","Nguyễn Bảo Uyên", true));
+        dsSinhVien.add(new SinhVien("SV2","Phạm Ânh Nhật", false));
+        dsSinhVien.add(new SinhVien("SV3","Trần Thiij Hương Thủy", true));
+        dsSinhVien.add(new SinhVien("SV4","Lê Phan Bảo Ngọc", true));
+        dsSinhVien.add(new SinhVien("SV5","Lê Tường Vy", true));
+        dsSinhVien.add(new SinhVien("SV6","Nguễn Bảo Hân", true));
+        dsSinhVien.add(new SinhVien("SV7","Trần Lê Thanh Tùng", false));
+        dsSinhVien.add(new SinhVien("SV8","Đàm Viết Trọng", false));
+        adapterSinhVien = new AdapterSinhVien(
+                MainActivity.this,
+                R.layout.item_viewsinhvien,
+                dsSinhVien
+        );
+        lv_HienThiDS.setAdapter(adapterSinhVien);
     }
 }
--- Cau1_QuanLySV/app/src/main/java/ntu/nguyenbaouyen63131662/adapter/AdapterSinhVien.java
+++ Cau1_QuanLySV/app/src/main/java/ntu/nguyenbaouyen63131662/adapter/AdapterSinhVien.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -40,8 +41,18 @@ public class AdapterSinhVien extends ArrayAdapter<SinhVien> {
         LayoutInflater LayoutInflater = this.context.getLayoutInflater();
         //đối sood this.resource chính là item.xml, te truyền vvaof khi gọi adaptersinhvien
         row = LayoutInflater.inflate(this.resource, null);
-        ImageView _img = row.findViewById(R.id.imageView2);
+        ImageView _img = row.findViewById(R.id.imageView4);
         TextView _Profile = row.findViewById(R.id.profile);
+        CheckBox _Checkbox = row.findViewById(R.id.checkBox1);
+
+        SinhVien sinhVien = this.objects.get(position);
+
+        if(sinhVien.getGioitinh()==true){
+            _img.setImageResource(R.drawable.nu);
+        }else {
+            _img.setImageResource(R.drawable.nam);
+        }
+        _Profile.setText(sinhVien.get_ID()+" - "+sinhVien.get_Name());
         return row;
     }
 }
