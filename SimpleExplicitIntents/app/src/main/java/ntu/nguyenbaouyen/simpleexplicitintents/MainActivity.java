d0e3789768453dbadb5dae87440b79b67f4b3521
NguyenBaoUyen <145897661+NguyenBaoUyen@users.noreply.github.com>
2024-04-21 12:18:39 +0700

    Hoàn Thành

--- SimpleExplicitIntents/app/src/main/java/ntu/nguyenbaouyen/simpleexplicitintents/MainActivity.java
+++ SimpleExplicitIntents/app/src/main/java/ntu/nguyenbaouyen/simpleexplicitintents/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu.nguyenbaouyen.simpleexplicitintents;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+        //Hàm đáp ứng sự kiện nhấn lên nút "Sang màn hình khác"
+        //Xử lý chuyển màn hình
+        public void ChuyenManHinh( View v){
+            //Tạo một đối tượng Intent
+            Intent iManHinhKhac = new Intent(this,SubActivityOne.class);
+            startActivity(iManHinhKhac);
+        }
     }
-}
